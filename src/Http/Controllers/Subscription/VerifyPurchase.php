2d891d8eec583b85bc2a391b7e7cd4c94fbed950
saeid <saeidkhakbazan@gmail.com>
2024-03-21 00:30:36 +0330

    feat: add atomic lock to verify purchase

--- src/Http/Controllers/Subscription/VerifyPurchase.php
+++ src/Http/Controllers/Subscription/VerifyPurchase.php
@@ -10,29 +10,43 @@ use IICN\Subscription\Services\Purchase\Appstore;
 use IICN\Subscription\Services\Purchase\Playstore;
 use IICN\Subscription\Services\Purchase\Purchase;
 use IICN\Subscription\Services\Response\SubscriptionResponse;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Contracts\Cache\LockTimeoutException;
 
 class VerifyPurchase extends Controller
 {
     public function __invoke(VerifyPurchaseRequest $request)
     {
-        $subscriptionTransaction = SubscriptionTransaction::query()->where('order_id', $request->validated('orderId'))->first();
+        $lock = Cache::lock('transaction_order_id_' . $request->validated('orderId'), 10);
 
-        if ($subscriptionTransaction) {
-            $message = $this->getMessage($subscriptionTransaction);
-            return SubscriptionResponse::data(['purchase_status' => $subscriptionTransaction->status], $message);
+        try {
+            $lock->block(5);
+
+            $subscriptionTransaction = SubscriptionTransaction::query()->where('order_id', $request->validated('orderId'))->first();
+
+            if ($subscriptionTransaction) {
+                $message = $this->getMessage($subscriptionTransaction);
+                return SubscriptionResponse::data(['purchase_status' => $subscriptionTransaction->status], $message);
+            }
+
+            if ($request->gateway == 'appStore') {
+                $playstore = new Purchase(new Appstore());
+            } elseif($request->gateway == 'playStore') {
+                $playstore = new Purchase(new Playstore());
+            } else {
+                return SubscriptionResponse::data(['purchase_status' => Status::FAILED], trans('subscription::messages.payment_not_valid'));
+            }
+
+            $result = $playstore->verify($request->skuCode, $request->purchaseToken, $request->orderId, $request->price);
+
+            $message = $this->getMessage($result['transaction']);
+
+            return SubscriptionResponse::data(['purchase_status' => $result['transaction']->status], $message);
+        } catch (LockTimeoutException $e) {
+            return SubscriptionResponse::data(['purchase_status' => Status::FAILED], trans('subscription::messages.payment_not_valid'));
+        } finally {
+            $lock?->release();
         }
-
-        if ($request->gateway == 'appStore') {
-            $playstore = new Purchase(new Appstore());
-        } elseif($request->gateway == 'playStore') {
-            $playstore = new Purchase(new Playstore());
-        }
-
-        $result = $playstore->verify($request->skuCode, $request->purchaseToken, $request->orderId, $request->price);
-
-        $message = $this->getMessage($result['transaction']);
-
-        return SubscriptionResponse::data(['purchase_status' => $result['transaction']->status], $message);
     }
 
     private function getMessage($transaction): string
